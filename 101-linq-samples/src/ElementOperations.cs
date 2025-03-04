12776dd65e113d020cea75b6fc25398c56716eda
Md-Faisal-Mahmud <md.faisal.mahmud.se@gmail.com>
2025-03-04 16:05:20 +0600

    .

--- 101-linq-samples/src/ElementOperations.cs
+++ 101-linq-samples/src/ElementOperations.cs
@@ -29,7 +29,9 @@ namespace Try101LinqSamples
             #region first-matching-element
             string[] strings = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
 
+            #region MyRegion
             string startsWithO = strings.First(s => s[0] == 'o');
+            #endregion
 
             Console.WriteLine($"A string starting with 'o': {startsWithO}");
             #endregion
@@ -41,7 +43,9 @@ namespace Try101LinqSamples
             #region first-or-default
             int[] numbers = { };
 
+            #region MyRegion
             int firstNumOrDefault = numbers.FirstOrDefault();
+            #endregion
 
             Console.WriteLine(firstNumOrDefault);
             #endregion
@@ -53,7 +57,9 @@ namespace Try101LinqSamples
             #region first-matching-or-default
             List<Product> products = GetProductList();
 
+            #region MyRegion
             Product product789 = products.FirstOrDefault(p => p.ProductID == 789);
+            #endregion
 
             Console.WriteLine($"Product 789 exists: {product789 != null}");
             #endregion
@@ -65,15 +71,16 @@ namespace Try101LinqSamples
             #region element-at
             int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
 
-            int fourthLowNum = (
+            var x = (
                 from n in numbers
                 where n > 5
-                select n)
-                .ElementAt(1);  // second number is index 1 because sequences use 0-based indexing
+                select n);
+
+            int fourthLowNum = x.ElementAt(1);  // second number is index 1 because sequences use 0-based indexing
 
             Console.WriteLine($"Second number > 5: {fourthLowNum}");
             #endregion
-            return 0;
+            return  0;
         }
     }
 }
