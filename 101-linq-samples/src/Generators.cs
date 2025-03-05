8f891d51795124a69dfb3bcf2c7e0560879b1e21
Md-Faisal-Mahmud <md.faisal.mahmud.se@gmail.com>
2025-03-05 10:37:33 +0600

    .

--- 101-linq-samples/src/Generators.cs
+++ 101-linq-samples/src/Generators.cs
@@ -11,9 +11,18 @@ namespace Try101LinqSamples
             #region MyRegion
             var numbers = from n in Enumerable.Range(100, 50)
                           select (Number: n, OddEven: n % 2 == 1 ? "odd" : "even");
+
+            var x = Enumerable
+                .Range(0, 10)
+                .Select(x =>
+                {
+                    if (x % 2 == 1)
+                        return (x, "odd");
+                    else
+                        return (x, "even");
+                });
             #endregion
 
-            var x = Enumerable.Range(100, )
             foreach (var n in numbers)
             {
                 Console.WriteLine("The number {0} is {1}.", n.Number, n.OddEven);
@@ -25,7 +34,9 @@ namespace Try101LinqSamples
         public int RepeatNumber()
         {
             #region generate-repeat
+            #region MyRegion
             var numbers = Enumerable.Repeat(7, 10);
+            #endregion
 
             foreach (var n in numbers)
             {
